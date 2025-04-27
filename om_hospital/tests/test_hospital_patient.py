60ba742c9b4063b819309686ab2e0d316ab883aa
louisoporto042 <louisoporto042@csu.fullerton.edu>
2025-04-27 16:51:07 -0700

    New testing of appointment functions

--- om_hospital/tests/test_hospital_patient.py
+++ om_hospital/tests/test_hospital_patient.py
@@ -8,19 +8,24 @@ class TestHospitalPatient(TransactionCase):
                                                                  'age': 50,
                                                                  'note': 'bike accident'})
 
+    def test_compute_appointment_count(self):
+        self.test_patient._compute_appointment_count()
+        self.assertEqual(self.test_patient.appointment_count, 0, "Appointment count should be 0")
+
     def test_get_name(self):
         name = self.test_patient.name_get()
         self.assertEqual(name[0][1], f'[{self.test_patient.reference}] John Doe', 'Name should be equal to "[Reference Num] John Doe"')
 
-    # def test_upper(self):
-    #     self.assertNotEqual('foo'.upper(), 'FOO')
+    def test_open_appointments(self):
+        result = self.test_patient.action_open_appointments()
+        self.assertEqual(result['type'], 'ir.actions.act_window')
+        self.assertEqual(result['name'], 'Appointments')
+        self.assertEqual(result['res_model'], 'hospital.appointment')
+        self.assertEqual(result['view_mode'], 'tree,form')
+        self.assertEqual(result['target'], 'current')
 
-    # def test_isupper(self):
-    #     self.assertTrue('FOO'.isupper(), 'Testing: Should be true')
-    #     self.assertFalse('foo'.islower(), 'Testing: Should be false')
-
-    # def test_split(self):
-    #     s = 'hello world'
-    #     self.assertEqual(s.split(), ['hello', 'world'])
-    #     with self.assertRaises(TypeError):
-    #         s.split(2)
+    def test_is_draft(self):
+        self.assertNotEqual('confirm', self.test_patient.state)
+        self.assertNotEqual('done', self.test_patient.state)
+        self.assertNotEqual('cancel', self.test_patient.state)
+        self.assertEqual('draft', self.test_patient.state)
