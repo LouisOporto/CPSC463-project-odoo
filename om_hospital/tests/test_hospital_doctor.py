add0331d24459b941cc31d9dbd1d8d9d37b7fde2
louisoporto042 <louisoporto042@csu.fullerton.edu>
2025-04-27 17:18:16 -0700

    Temporary design of selenium testcase (requires selenium and odoo-selenium

--- om_hospital/tests/test_hospital_doctor.py
+++ om_hospital/tests/test_hospital_doctor.py
@@ -1,12 +1,24 @@
-from odoo.tests.common import TransactionCase
+from odoo.tests import tagged
+from odoo_selenium import SeleniumCase
 
-class TestHospitalDoctor(TransactionCase):
+from selenium.webdriver.common.by import By
+
+@tagged("-at_install", "post_install")
+class TestHospitalDoctor(SeleniumCase):
 
     def setUp(self, *args, **kwargs):
         super(TestHospitalDoctor, self).setUp(*args, **kwargs)
 
-    # def test_upper(self):
-    #     self.assertNotEqual('foo'.upper(), 'FOO')
+
+    def test_login(self):
+        self.navigate("/web/login")
+        email_input = self.driver.find_element(By.ID, "login")
+        email_input.send_keys("louisoporto042@csu.fullerton.edu")
+        password_input = self.driver.find_element(By.ID, "password")
+        password_input.send_keys("odoo15")
+        login_button = self.driver.find_element(By.ID, "submit")
+        login_button.click()
+        self.assertTrue(self.driver.find_element(By.ID, "user_menu").is_displayed())
 
     # def test_isupper(self):
     #     self.assertTrue('FOO'.isupper(), 'Testing: Should be true')
